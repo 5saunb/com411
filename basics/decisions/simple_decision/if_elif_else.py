063a30bb2e733e93639a346bff92aa3a901bfdb4
repl.it user <replituser@example.com>
2020-10-06 10:07:28 +0000

    Added elif statement file

---
+++ basics/decisions/simple_decision/if_elif_else.py
@@ -0,0 +1,18 @@
+#User prompt for direction of painbrush
+print("Beep, the robot, is going to try and paint a picture. Which direction should he move his brush in? (up, down, left or right?")
+direction = input()
+
+if (direction == "up"):
+  print ("I'm painting upwards!")
+
+elif direction == "down" :
+  print ("I'm going to paint down!")
+
+elif direction == "right" :
+  print("Right it is then!")
+
+elif direction == "left":
+  print("left we go!")
+
+print("Analysis complete!")
+
