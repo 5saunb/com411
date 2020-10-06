c8a11f3661d6a3ca4c90267188a1ba1314e4d406
repl.it user <replituser@example.com>
2020-10-06 10:25:03 +0000

    Did modulo operator code thing

---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,12 @@
+# Prompt User to enter a whole number
+print("Would you kindly enter a whole number")
+number = int(input())
+
+# Working out if odd/even 
+if (number % 2 !=0):
+  print("The number", number, "is odd!")
+
+else:
+  print ("The number", number, "is even!")
+
+print("Computations completed!")
