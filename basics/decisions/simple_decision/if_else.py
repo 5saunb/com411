b776c09d2947450ed29738fa42af6bac546e005f
repl.it user <replituser@example.com>
2020-10-06 09:43:10 +0000

    Added code to demonstrate if_else

---
+++ basics/decisions/simple_decision/if_else.py
@@ -0,0 +1,14 @@
+# Prompt user for input
+print("Please enter the activity to be performed")
+activity = input()
+
+if activity == "calculations":
+  print("Performing calculations...")
+
+else : print("Performing Activity")
+
+print(activity, "Completed")
+
+if activity =="ball waxing":
+  print("Ouch!")
+  
