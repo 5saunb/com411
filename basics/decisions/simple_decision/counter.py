58da9706d088485878c4b20114079095ae682ac1
repl.it user <replituser@example.com>
2020-10-12 16:28:30 +0000

    Updated

---
+++ basics/decisions/simple_decision/counter.py
@@ -0,0 +1,11 @@
+#First User input
+print("Please enter the first whole number")
+num1 = int(input())
+
+#Second User input
+print("Please enter the second whole number")
+num2 = int(input())
+
+#Third User input
+print("Please enter the third whole number")
+num3 = int(input())
