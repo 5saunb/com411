61a48722608c7a038b3489bd3b260f29a1198c3e
repl.it user <replituser@example.com>
2020-10-13 10:02:43 +0000

    Created single-parameter functionbs

--- basics/functions/function_with_loop.py
+++ basics/functions/function_with_loop.py
@@ -1,3 +1,19 @@
-#Define Function Crossed Step
-def cross_bridge():
-  
+#Define Function 
+def cross_bridge(steps):
+  num = 0
+  while (steps > num):
+    print("Crossed Step.")
+    num = num +1
+  if steps > 5:
+    print("The bridge is collapsing!")
+  else:
+    print("We must keep going!")
+
+cross_bridge(3)
+cross_bridge(6)
+
+
+print("The bridge is collapsing!")
+
+
+git 
