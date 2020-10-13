296975a0af0f888ce3b04fc393f3add961444440
repl.it user <replituser@example.com>
2020-10-13 10:07:21 +0000

    Added function with multiple parameters

---
+++ basics/functions/function_with_parameters.py
@@ -0,0 +1,9 @@
+#define function
+def climb_ladder (steps,steps_crossed):
+  if steps > steps_crossed:
+    print("Still some way to go!")
+  else:
+    print("We are almost there!")
+
+climb_ladder(5,2)
+climb_ladder(2,5)
